d96db6bbb3ed2bf074a1dab697be430c34c8eef6
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-09 16:56:19 -0300

    Finalizando a barra de navegação

--- src/Components/navbar/Navbar.jsx
+++ src/Components/navbar/Navbar.jsx
@@ -15,24 +15,44 @@ const Navbar = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">Home</a>
+                <a className="nav-link active text-white" aria-current="page" href="#">
+                  Home <i className="fas fa-bolt"></i>
+                </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="#">Link</a>
+                <a className="nav-link text-white" href="#">
+                  A Faisca   <i className="fas fa-bolt"></i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  A Corrente <i className="fas fa-bolt"></i>
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-white" href="#">
+                  A era Moderna <i className="fas fa-bolt"></i>
+                </a>
               </li>
               <li className="nav-item dropdown">
-                <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  Dropdown
+                <a className="nav-link text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Cards <i className="fas fa-bolt"></i>
                 </a>
                 <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="#">Action</a></li>
-                  <li><a className="dropdown-item" href="#">Another action</a></li>
+                  <li><a className="dropdown-item" href="#">
+                    Cientitas Da Primeira <span className="text-danger">Era</span>
+                    <i className="fas fa-bolt"></i>
+                  </a></li>
+                  <li><a className="dropdown-item" href="#">
+                    Cientitas Da Segunda <span className="text-danger">Era</span>
+                    <i className="fas fa-bolt"></i>
+                  </a></li>
 
-                  <li><a className="dropdown-item" href="#">Something else here</a></li>
+                  <li><a className="dropdown-item" href="#">
+                    Cientitas Da Terceira <span className="text-danger">Era</span>
+                    <i className="fas fa-bolt"></i>                    
+                  </a></li>
                 </ul>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link disabled">Disabled</a>
               </li>
             </ul>
 
