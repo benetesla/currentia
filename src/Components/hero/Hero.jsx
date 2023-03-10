60e289e92a772a5005e0d341be88848745a2fbff
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-03-09 21:01:46 -0300

    Inicio da codificacao dos cards, criando a hero Sction em nossa home page, nessa pagina temos um cartao com alguns efeitos de pseudo elementos do css para da um upgrade no sistema, tambem temos o nosso primeiro link

--- src/Components/hero/Hero.jsx
+++ src/Components/hero/Hero.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
+import './Hero.css'
+import Eltrici from '../../IMG/Eletri.jfif'
 
 const Hero = () => {
+
   return (
-    <div>Home</div>
+    <div className="hero">
+      <article className="card">
+        <div className="temporary_text">
+          <img src={Eltrici} alt="Eltrici" />
+        </div>
+        <div className="card_content">
+          <span className="card_title">
+            A História da Eletricidade
+          </span>
+          <span className="card_subtitle">
+            O começo da eletricidade
+            </span>
+          <p className="card_description">
+            A História da eletricidade tem
+            seu início no século VI a.C., na Grécia Antiga,
+            quando o filósofo Thales de Mileto, após descobrir
+            uma resina vegetal fóssil petrificada chamada
+            âmbar (elektron em grego), esfregou-a com
+            pele e lã de animais e pôde então observar
+            seu poder de atrair objetos leves como palhas,
+             fragmentos de madeira e penas.
+          </p>
+        </div>
+      </article>
+      <button className="btn btn-primary">
+        <a href="/history">Saiba mais</a>
+      </button>
+
+    </div>
+
   )
 }
 
